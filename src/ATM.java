c3aab1593b922dc706fb3ffba35ae01fc3cdf0b1
KevinBozic <bozic.kevin@gmail.com>
2016-02-04 16:08:21 -0500

    Added loops

--- src/ATM.java
+++ src/ATM.java
@@ -1,5 +1,6 @@
 import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -9,9 +10,34 @@ public class ATM {
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-        System.out.println("South Carolina FCU");
+        System.out.println("SC Federal Credit Union");
         User newUser = new User();
         newUser.loginName();
-        newUser.choice();
+
+        HashMap<String, Double> member = new HashMap<>();
+        //member.put("Kevin", balance);
+
+
+        if (!member.containsKey(newUser.name)) {
+            System.out.println("It seems as though we don't have an account under that name. Would you like to create a new account? [y/n]");
+            String existingAccount = ATM.scanner.nextLine();
+
+            if (existingAccount.equalsIgnoreCase("y")) { // need to loop this if user doesn't type y or n
+                System.out.println("Please enter your first name to create an account:");
+                String newAccount = ATM.scanner.nextLine();
+                System.out.println("Welcome, " + newAccount + "!");
+                member.put(newAccount, 100.0);
+            } else if (existingAccount.equalsIgnoreCase("n")) {
+                System.out.println("We're sorry to see you go. Have a wonderful day!");
+                System.exit(0);
+            } else {
+                System.out.println("This doesn't appear to be a valid input. Please type 'y' or 'n'");
+
+            }
+        }
+
+        while (true) {
+            newUser.choice();
         }
     }
+}
--- src/User.java
+++ src/User.java
@@ -1,35 +1,45 @@
+import java.util.HashMap;
+
 /**
  * Created by KevinBozic on 2/3/16.
  */
 public class User {
-    int balance = 100;
+    int balance;
     String name;
     String choice;
     int amount;
 
     public void loginName() {
-        System.out.println("What is your name?");
+        System.out.println("Please enter your first name here:");
         name = ATM.scanner.nextLine();
-        System.out.println("Welcome, " + name + ".");
+        System.out.println("Hi there, " + name + ".");
     }
 
     public void choice() throws Exception {
-        System.out.println("What would you like to do? [check balance / withdraw / cancel]");
+        System.out.println("What would you like to do? [check balance / withdraw / deposit / cancel]");
         choice = ATM.scanner.nextLine();
 
         if (choice.equalsIgnoreCase("check balance")) {
-            System.out.println("Your balance is " + balance + ".");
+            System.out.println("Your balance is " + "$" + balance + ".");
         } else if (choice.equalsIgnoreCase("cancel")) {
-            System.out.println("Thank you and come again!");
+            System.out.println("Thank you for banking with SC Federal Credit Union. Have a great day!");
+            System.exit(0);
         } else if (choice.equalsIgnoreCase("withdraw")) {
             System.out.println("How much would you like to withdraw?");
-            amount = ATM.scanner.nextInt();
+            amount = Integer.valueOf(ATM.scanner.nextLine());
             if (amount > balance) {
                 throw new Exception("Insufficient Funds");
             } else {
                     balance = balance - amount;
-                System.out.println("Withdrawal successful. You now have " + balance + " in your account. Have a nice day!");
+                System.out.println("Withdrawal successful. You now have " + "$"+ balance + " remaining in your account.");
             }
+        } else if (choice.equalsIgnoreCase("deposit")) {
+            System.out.println("How much would you like to deposit?");
+            amount = Integer.valueOf(ATM.scanner.nextLine());
+            balance = balance + amount;
+                System.out.println("Deposit successful. You now have " + "$"+ balance + " currently in your account.");
+        } else {
+            System.out.println("There doesn't appear to be an option for that. Please select another:");
         }
     }
 }
